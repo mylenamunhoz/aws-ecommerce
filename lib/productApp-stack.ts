0bafda5075753ec0565885c544d9358468726af5
Mylena Munhoz <mylena.prado@edu.unipar.br>
2022-09-28 09:08:47 -0300

    create a function lambda for admin

--- lib/productApp-stack.ts
+++ lib/productApp-stack.ts
@@ -6,6 +6,7 @@ import { Construct } from "constructs"
 
 export class ProductsAppStack extends cdk.Stack {
   readonly productsFetchHandler: lambdaNodeJS.NodejsFunction
+  readonly productsAdminHandler: lambdaNodeJS.NodejsFunction
   readonly productsDdb: dynamodb.Table
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -23,17 +24,38 @@ export class ProductsAppStack extends cdk.Stack {
       writeCapacity: 1
     })
     this.productsFetchHandler = new lambdaNodeJS.NodejsFunction(this, 
-      "ProductsFetchFunction", {
-        functionName: "ProductsFetchHandler",
-        entry: "lambda/products/productsFetchFunction.ts",
-        handler: "handler",
-        memorySize: 128,
-        timeout: cdk.Duration.seconds(5),
-        bundling: {
-          minify: true,
-          sourceMap: false
-        }
+    "ProductsFetchFunction", {
+      functionName: "ProductsFetchFunction",
+      entry: "lambda/products/productsFetchFunction.ts",
+      handler: "handler",
+      memorySize: 128,
+      timeout: cdk.Duration.seconds(5),
+      bundling: {
+        minify: true,
+        sourceMap: false
+      },
+      environment: {
+        PRODUCTS_DDB: this.productsDdb.tableName
       }
-    )
+    })
+
+    this.productsDdb.grantReadData(this.productsFetchHandler)
+
+    this.productsAdminHandler = new lambdaNodeJS.NodejsFunction(this, 
+    "ProductsAdminFunction", {
+      functionName: "ProductsAdminFunction",
+      entry: "lambda/products/productsAdminFunction.ts",
+      handler: "handler",
+      memorySize: 128,
+      timeout: cdk.Duration.seconds(5),
+      bundling: {
+        minify: true,
+        sourceMap: false
+      },
+      environment: {
+        PRODUCTS_DDB: this.productsDdb.tableName
+      }
+    })
+    this.productsDdb.grantWriteData(this.productsAdminHandler)
   }
 }
